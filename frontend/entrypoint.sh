7e5a0ba02361a40a6135e7861329731fe4dea5c9
EcoClean <dev@ecoclean.com>
2026-06-08 18:01:05 -0500

    Fix entrypoint.sh sed compatibility with Alpine/BusyBox

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 BACKEND_HOST="${BACKEND_HOST:-backend}"
 echo "Configuring nginx to proxy /api to ${BACKEND_HOST}:4000"
 
-sed -i "s/__BACKEND_HOST__/${BACKEND_HOST}/g" /etc/nginx/conf.d/default.conf
+sed -i.bak "s/__BACKEND_HOST__/${BACKEND_HOST}/g" /etc/nginx/conf.d/default.conf
+rm -f /etc/nginx/conf.d/default.conf.bak
 
 exec nginx -g "daemon off;"
