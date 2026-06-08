2e618247a5f94cae000f5e1033247de029563ad3
EcoClean <dev@ecoclean.com>
2026-06-08 18:09:22 -0500

    Fix: dynamic nginx resolver + simplified entrypoint

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -e
 
 BACKEND_HOST="${BACKEND_HOST:-backend}"
-echo "Configuring nginx to proxy /api to ${BACKEND_HOST}:4000"
+echo "entrypoint: BACKEND_HOST=${BACKEND_HOST}"
 
 sed -i.bak "s/__BACKEND_HOST__/${BACKEND_HOST}/g" /etc/nginx/conf.d/default.conf
 rm -f /etc/nginx/conf.d/default.conf.bak
 
+echo "entrypoint: nginx config ready, starting..."
 exec nginx -g "daemon off;"
